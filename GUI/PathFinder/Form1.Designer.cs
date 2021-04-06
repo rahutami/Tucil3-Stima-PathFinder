445996a23aa1df4af27979cec30ab3db13339b84
nabilaherfar <13519181@std.stei.itb.ac.id>
2021-04-06 15:53:03 +0700

    add GUI

---
+++ GUI/PathFinder/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace PathFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.button1.Location = new System.Drawing.Point(240, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(80, 140);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(473, 228);
+            this.panel1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(80, 416);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(352, 416);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(201, 23);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(590, 140);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(428, 228);
+            this.panel2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(334, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 16);
+            this.textBox1.TabIndex = 7;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("Perpetua Titling MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox2.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.richTextBox2.Location = new System.Drawing.Point(466, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(242, 38);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "Path Finder";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Font = new System.Drawing.Font("Perpetua Titling MT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox3.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.richTextBox3.Location = new System.Drawing.Point(80, 75);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(151, 20);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "Masukkan File :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.textBox3.Location = new System.Drawing.Point(80, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(163, 22);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "Visualisasi Peta";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.textBox4.Location = new System.Drawing.Point(590, 113);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(191, 22);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "The Shortest Path :";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.richTextBox1.Location = new System.Drawing.Point(80, 384);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(151, 26);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "Pilih tujuan awal : ";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox4.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.richTextBox4.Location = new System.Drawing.Point(352, 389);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(151, 25);
+            this.richTextBox4.TabIndex = 14;
+            this.richTextBox4.Text = "Pilih tujuan akhir : ";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox5.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox5.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.richTextBox5.Location = new System.Drawing.Point(590, 409);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(178, 30);
+            this.richTextBox5.TabIndex = 15;
+            this.richTextBox5.Text = "";
+            this.richTextBox5.TextChanged += new System.EventHandler(this.richTextBox5_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.textBox2.Location = new System.Drawing.Point(594, 389);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 16);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "Distance :";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.richTextBox6.Location = new System.Drawing.Point(901, 447);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(83, 18);
+            this.richTextBox6.TabIndex = 17;
+            this.richTextBox6.Text = "";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBox5.Location = new System.Drawing.Point(677, 460);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(91, 16);
+            this.textBox5.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1053, 477);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ GUI/PathFinder/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PathFinder
+{
+    public partial class Form1 : Form
+    {
+        private List<Node> nodes;
+        private List<List<bool>> adjMatrix;
+        public string[] adjNode;
+        public string[] identity;
+        private Graph map;
+        private Node node;
+        private List<Node> path;
+        double distance;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            //string readfile = File.ReadAllText(filename);
+            textBox1.Text = filename;
+
+            map = new Graph(filename);
+
+            List<string> a = map.GetNodeNames();
+            List<string> b = map.GetNodeNames();
+            comboBox1.DataSource = a;
+            comboBox2.DataSource = b;
+
+
+            nodes = new List<Node>();
+
+            StreamReader graphFile = new StreamReader(filename);
+            int n;
+
+            Int32.TryParse(graphFile.ReadLine(), out n);
+
+            for (int i = 1; i <= n; i++)
+            {
+                double latitude, longitude;
+                string name = "";
+
+                // format per line latitude longitude Nama
+                identity = graphFile.ReadLine().Split(" ");
+
+                latitude = Double.Parse(identity[0], System.Globalization.CultureInfo.InvariantCulture);
+                longitude = Double.Parse(identity[1], System.Globalization.CultureInfo.InvariantCulture);
+
+                // Double.TryParse(identity[0], out latitude);
+                // Double.TryParse(identity[1], out longitude);
+
+                for (int j = 2; j < identity.Length; j++)
+                {
+                    name += identity[j];
+
+
+                    if (j != identity.Length - 1) name += " ";
+                   
+                }
+
+                node = new Node(name, i, latitude, longitude);
+                InsertNode(node);
+            }
+
+            List<List<bool>> adjMatrix = new List<List<bool>>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                adjNode = graphFile.ReadLine().Split(" ");
+                List<bool> adjMatrixRow = new List<bool>();
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (adjNode[j] == "1")
+                    {
+                        GetNode(i + 1).insertAdjNode(j + 1);
+                        adjMatrixRow.Add(true);
+
+                    }
+                    else
+                    {
+                        adjMatrixRow.Add(false);
+                    }
+                }
+                adjMatrix.Add(adjMatrixRow);
+
+
+            }
+
+            
+            //MSAGL
+            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graphh = new Microsoft.Msagl.Drawing.Graph("graphh");
+            //create the graph content 
+
+            foreach (Node node in nodes)
+            {
+                foreach (int adjNode in node.GetAdjList())
+                {
+                    if (node.GetID() < GetNode(adjNode).GetID()) { 
+                        var Edge = graphh.AddEdge(node.GetName(), GetNode(adjNode).GetName());
+                        Edge.Attr.ArrowheadAtTarget = Microsoft.Msagl.Drawing.ArrowStyle.Diamond;
+                    }
+                }
+            }
+
+            viewer.Graph = graphh;
+            //associate the viewer with the form 
+            panel1.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Controls.Add(viewer);
+            panel1.ResumeLayout();
+            InsertAdjMatrix(adjMatrix);
+
+            graphFile.Close();
+
+        }
+
+        public List<string> GetNodeNames()
+        {
+            List<string> names = new List<string>();
+
+            foreach (Node node in nodes)
+            {
+                names.Add(node.GetName());
+            }
+
+            return names;
+        }
+        public void InsertAdjMatrix(List<List<bool>> adjMatrix)
+        {
+            this.adjMatrix = new List<List<bool>>(adjMatrix);
+        }
+
+        private void InsertNode(Node node)
+        {
+            nodes.Add(node);
+        }
+
+        private Node GetNode(int id)
+        {
+            foreach (Node node in nodes)
+            {
+                if (node.GetID() == id) return node;
+            }
+            return null;
+        }
+
+        private Node GetNode(string name)
+        {
+            foreach (Node node in nodes)
+            {
+                if (node.GetName().ToLower() == name.ToLower()) return node;
+            }
+            return null;
+        }
+
+        public void PrintNodes()
+        {
+            foreach (Node node in nodes)
+            {
+                Console.Write(node.GetName() + " ");
+                foreach (int adjID in node.GetAdjList())
+                {
+                    Console.Write(GetNode(adjID).GetName() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (Node node in nodes)
+            {
+                node.SetDistanceFromStart(-1);
+                node.SetEstimatedDistance(-1);
+                node.SetParentID(0);
+                node.UnVisit();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            panel2.Controls.Clear();
+
+            int selectedIndex1 = comboBox1.SelectedIndex;
+            Object selectedItem = comboBox1.SelectedItem;
+            richTextBox6.Text = selectedItem.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            panel2.Controls.Clear();
+            int selectedIndex2 = comboBox2.SelectedIndex;
+            Object selectedItem2 = comboBox2.SelectedItem;
+            //string start = richTextBox1.Text;
+            //string destination = selectedItem2.ToString();
+            textBox5.Text = selectedItem2.ToString();
+           
+
+            PathFinder findPath = new PathFinder(map.GetNode(richTextBox6.Text).GetName(), map.GetNode(textBox5.Text).GetName(), map);
+
+            Node startNode = map.GetNode(map.GetNode(richTextBox6.Text).GetName());
+            Node destNode = map.GetNode(map.GetNode(textBox5.Text).GetName());
+            Node currentNode = startNode;
+            SortedQueue queue = new SortedQueue();
+            currentNode.SetDistanceFromStart(0);
+            currentNode.SetEstimatedDistance(currentNode.CalculateDistance(destNode));
+            currentNode.Visit();
+            while (currentNode != null)
+            {
+                foreach (int adjID in currentNode.GetAdjList())
+                {
+                    currentNode.Visit();
+                    Node adjNode = map.GetNode(adjID);
+
+                    if (adjNode.CalculateDistance(currentNode) + adjNode.CalculateDistance(destNode) + currentNode.GetDistanceFromStart() < adjNode.GetEstimatedDistance() || adjNode.GetEstimatedDistance() == -1)
+                    {
+                        adjNode.SetParentID(currentNode.GetID());
+
+                        adjNode.SetDistanceFromStart(adjNode.CalculateDistance(currentNode) + currentNode.GetDistanceFromStart());
+
+                        adjNode.SetEstimatedDistance(adjNode.GetDistanceFromStart() + adjNode.CalculateDistance(destNode));
+                    }
+
+                    // Kalo node belom divisit dan node bukan destination maka dimasukkin ke queue
+                    if (!adjNode.GetVisited() && !Equals(adjNode, destNode))
+                    {
+                        queue.Enqueue(adjNode);
+                    }
+                }
+                currentNode = queue.Dequeue();
+            }
+           
+
+            Node currentNodee = destNode;
+            path = new List<Node>();
+
+            if (destNode.GetParentID() != 0)
+            {
+                do
+                {
+                    path.Add(currentNodee);
+                    currentNodee = map.GetNode(currentNodee.GetParentID());
+                } while (!Equals(startNode, currentNodee));
+                path.Add(currentNodee);
+                path.Reverse();
+            }
+            distance = destNode.GetDistanceFromStart();
+            map.Clear();
+
+            //MSAGL
+            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graphhh = new Microsoft.Msagl.Drawing.Graph("graphhh");
+            //create the graph content 
+
+            foreach (Node node1 in path)
+            {
+                foreach (int adjNode1 in node1.GetAdjList())
+                {
+                    if (node1.GetID() < GetNode(adjNode1).GetID())
+                    {
+                        var Edge = graphhh.AddEdge(node1.GetName(), GetNode(adjNode1).GetName());
+                        Edge.Attr.ArrowheadAtTarget = Microsoft.Msagl.Drawing.ArrowStyle.Diamond;
+                    }
+                }
+            }
+
+            viewer.Graph = graphhh;
+            //associate the viewer with the form 
+            panel2.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel2.Controls.Add(viewer);
+            panel2.ResumeLayout();
+
+            richTextBox5.Text = findPath.GetDistance().ToString();
+        }
+
+        
+        private void richTextBox5_TextChanged(object sender, EventArgs e){}
+    }
+}
